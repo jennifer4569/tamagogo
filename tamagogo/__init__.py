1e9cdffbb854f88cd9f69174c1c79f64907da805
Jake Zaia <jlz2369@g.rit.edu>
2019-02-16 15:29:09 -0500

    Mongo utils for login

--- tamagogo/__init__.py
+++ tamagogo/__init__.py
@@ -10,6 +10,17 @@ app.secret_key = os.urandom(16)
 def root():
     return render_template("home.html")
 
+
+@app.route("login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0")
---
+++ tamagogo/utils/mongo_utils.py
@@ -0,0 +1,36 @@
+from pymongo import MongoClient
+from hashlib import sha256
+
+
+client = MongoClient()
+db = client.tamagogo
+collection = db.users
+
+
+def create_new_user(username, password):
+    if (get_user(username) == None):
+        collection.insert(
+            {
+                "username": username,
+                "password": hash(username, password),
+                "totalScore": 0,
+                "currScore": 0,
+                "creaturesUnlocked": {}
+            }
+        )
+        return True
+    else:
+        return False
+
+def get_user(username):
+    return collection.find_one({"username": username})
+
+def hash(username, password):
+    return sha256(str(username+password).encode('utf-8')).hexdigest()
+
+def authenticate(username, password):
+    user = get_user(username)
+    if (user == None):
+        return False
+    else:
+        return user["password"] == hash(username, password)
