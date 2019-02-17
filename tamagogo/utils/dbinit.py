b50988af8009bb28f75fb57aed32f9096f03140e
Jake Zaia <jlz2369@g.rit.edu>
2019-02-16 20:52:18 -0500

    Push some more basic mongo utilities

---
+++ tamagogo/utils/dbinit.py
@@ -0,0 +1,6 @@
+from mongo_utils import *
+
+if __name__ == '__main__':
+    create_egg_entry("default", 1, "undecided")
+    create_deed_entry(1, "Hold the door", "Hold the door for a friend or a stranger", 0)
+    pass
--- tamagogo/utils/mongo_utils.py
+++ tamagogo/utils/mongo_utils.py
@@ -5,17 +5,20 @@ from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.tamagogo
-collection = db.users
+user_collection = db.users
+deed_collection = db.deeds
+egg_collection = db.eggs
 
 
 def create_new_user(username, password):
     if (get_user(username) == None):
-        user = collection.insert(
+        user = user_collection.insert(
             {
                 "username": username,
                 "password": hash(username, password),
                 "totalScore": 0,
                 "currScore": 0,
+                "currEgg": None,
                 "creaturesUnlocked": {}
             }
         )
@@ -24,7 +27,7 @@ def create_new_user(username, password):
         return False
 
 def get_user(username):
-    return collection.find_one({"username": username})
+    return user_collection.find_one({"username": username})
 
 def hash(username, password):
     return sha256(str(username+password).encode('utf-8')).hexdigest()
@@ -35,3 +38,39 @@ def authenticate(username, password):
         return False
     else:
         return user["password"] == hash(username, password)
+
+def check_hatch(username):
+    return get_user["currScore"] > 0
+
+def gen_new_egg(username):
+    user = get_use(username)
+    if user==None:
+        return False
+    if user["totalScore"] < 90:
+        pass
+    elif user["totalScore"] < 300:
+        pass
+    else:
+        pass
+
+
+def create_egg_entry(id_string, rarity, cname):
+    if (egg_collection.find_one({"id_string": id_string}) != None):
+        return False
+    egg_collection.insert({
+        "id_string": id_string,
+        "rarity": rarity,
+        "cname": cname
+    })
+    return True
+
+def create_deed_entry(worth, text, desc, id_num):
+    if (deed_collection.find_one({"id_num": id_num}) != None):
+        return False
+    deed_collection.insert({
+        "id_num": id_num,
+        "worth": worth,
+        "text": text,
+        "desc": desc
+    })
+    return True
